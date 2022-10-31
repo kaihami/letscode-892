7ed944b381ef4ae92781b84a0a6b4a9acc7565a9
Gilberto Kaihami <kaihami@gmail.com>
2022-10-31 20:05:00 -0300

    Adicionando subtracao, divisao, multiplicacao no arquivo funcoes.py

--- Tecnicas-programacao-I/projetos/calculadora/calculadora/funcoes.py
+++ Tecnicas-programacao-I/projetos/calculadora/calculadora/funcoes.py
@@ -1,4 +1,3 @@
-
 def soma(a, b):
     """
     args:
@@ -13,4 +12,47 @@ def soma(a, b):
         raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
 
 
-print(1)
+def subtracao(a, b):
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a - b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a - b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
+
+
+def divisao(a, b):
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a / b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        if b == 0:
+            print('Divisão invalida, recebido 0 como denominador')
+            return 0
+        else:
+            return a / b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
+
+
+def multiplicacao(a, b):
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a * b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a * b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
