755fd71dfb9f4c14ec06545e2a252cdffa3e3574
Gilberto Kaihami <kaihami@gmail.com>
2022-10-31 20:22:12 -0300

    Finalizando projeto calculadora

---
+++ Tecnicas-programacao-I/projetos/calculadora/calculadora/__init__.py
@@ -0,0 +1,25 @@
+from .funcoes import soma, subtracao, divisao, multiplicacao
+
+
+def calcule():
+    a = float(input('Insira um número '))
+    b = float(input('Insira um número '))
+    operacao = input('Insira uma operacao válida ')
+
+    switcher = {
+        "soma": soma,
+        "subtracao": subtracao,
+        "divisao": divisao,
+        "multiplicacao": multiplicacao,
+        "+": soma,
+        "-": subtracao,
+        "/": divisao,
+        "*": multiplicacao,
+    }
+    if operacao in switcher.keys():
+        resultado = switcher[operacao](a, b)
+    else:
+        raise KeyError(f'Operação desconhecida {operacao}, escolha entre {list(switcher.keys())}')
+    return resultado
+
+
